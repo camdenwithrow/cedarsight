5857a024643f9a9345a0d1a05ce7f0f3f1d494a7
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-11-25 00:35:36 -0500

    add file upload to openai

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const cors = require("cors")
 const clerk = require("@clerk/clerk-sdk-node")
+const { OpenAIApi, Configuration } = require('openai');
+const fs = require("fs")
 const multer = require("multer")
 require('dotenv').config()
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+//   });
+  
+const openaiClient = new OpenAIApi({apiKey: process.env.OPENAI_API_KEY,});
 
 const app = express()
 
@@ -14,9 +25,22 @@ app.get('/', (req, res) => {
 
 })
 
-app.get("/protected", clerk.ClerkExpressRequireAuth({
-}), (req, res) => {
-    res.json(req.auth)
+app.post("/upload", clerk.ClerkExpressRequireAuth({
+}), upload.array('files'), async (req, res) => {
+    let responses = []
+    try {
+        const files = req.files
+        for (const file of files) {
+            const resp = await openaiClient.files.create({
+                file: fs.createReadStream(file.buffer),
+                purpose: "assistants"
+            })
+            responses.push(resp)
+        }
+        res.send({message: 'Files uploaded successfully', openaiResps: responses})
+    } catch (error) {
+        res.status(500).send({ message: 'Error uploading files', error: error });
+    }
 })
 
 app.use((err, req, res, next) => {
