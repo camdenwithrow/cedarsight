bd2384591a8ae109f432927c0100ba3f0c6b0208
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-11-24 23:59:42 -0500

    fix clerk auth on server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const cors = require("cors")
 const clerk = require("@clerk/clerk-sdk-node")
+const multer = require("multer")
+require('dotenv').config()
+
+const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(clerk.ClerkExpressRequireAuth());
 
-app.post('/', (req, res) => {
-  const { userId, sessionId, getToken } = req.auth;
-  // Your handler...
+app.get('/', (req, res) => {
+    res.send({message: "Heyyy"})
+
 })
+
+app.get("/protected", clerk.ClerkExpressRequireAuth({
+}), (req, res) => {
+    res.json(req.auth)
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(401).send('Unauthenticated!')
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
