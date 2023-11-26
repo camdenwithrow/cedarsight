a65a0e4c22eca3106cf6edc1b189e03274acfc30
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-11-26 09:05:32 -0500

    files endpoint

--- server/index.ts
+++ server/index.ts
@@ -3,12 +3,16 @@ import * as dotenv from "dotenv"
 import cors from "cors"
 import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node"
 import OpenAI from "openai"
+import fs from "fs"
+import multer from "multer"
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 app.use(express.json())
 app.use(cors())
@@ -22,12 +26,32 @@ app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-app.get("/openai", async (req, res) => {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "give me a 5 word poem" }],
-    model: "gpt-3.5-turbo",
-  })
-  res.send(completion)
+// app.get("/openai", async (req, res) => {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "user", content: "give me a 5 word poem" }],
+//     model: "gpt-3.5-turbo",
+//   })
+//   res.send(completion)
+// })
+
+// TODO: add auth
+app.post("/upload", upload.array("files"), async (req, res) => {
+  let responses = []
+  try {
+    const files = req.files
+    if (!Array.isArray(files)) throw new Error("files not listed properly")
+    for (const file of files) {
+      responses.push(file.filename)
+      //   const aiResp = await openai.files.create({
+      //     file: fs.createReadStream(file.buffer),
+      //     purpose: "assistants",
+      //   })
+      //   responses.push(aiResp)
+    }
+    res.send({ message: "Files uploaded successfully", openaiResps: responses })
+  } catch (error) {
+    res.status(500).send({ message: "Error uploading files", error: error })
+  }
 })
 
 app.get("/protected", ClerkExpressRequireAuth({}), (req, res) => {
