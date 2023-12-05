99614dafefa7918807f89b2ce2d982523b08790a
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-12-04 21:16:36 -0600

    change model

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -67,7 +67,7 @@ router.post("/summarize/chat", async (req: Request, res: Response) => {
       ${req.body.file.text}
     `
   const msgResp = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-1106",
     messages: [{ role: "user", content: msgContent }],
   })
   console.log("openaiResp", msgResp)
